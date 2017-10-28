4ec68b6bb41d8a80c74e9d80b079e37bc5693827
Karan Desai <karandesai_96@live.com>
2017-10-28 09:43:10 +0530

    Remove unused variables from dataset class, tidy up a bit.

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import itertools
 import json
+import os
 import random
 
 import torch
@@ -8,13 +9,18 @@ from torch.utils.data import Dataset
 
 
 class ShapesQADataset(Dataset):
-    def __init__(self, opt):
-        self.opt = opt
-        if 'load_path' in opt:
+    def __init__(self, data_path, train_split, q_out_vocab, a_out_vocab):
+        self.path = data_path
+        self.train_split = train_split
+        self.q_out_vocab = q_out_vocab
+        self.a_out_vocab = a_out_vocab
+
+        if os.path.exists(self.path):
             # load dataset from file
-            with open(opt['load_path'], 'r') as file_id:
-                loaded = json.load(file_id)
+            with open(self.path, 'r') as infile:
+                loaded = json.load(infile)
                 for key, value in loaded.iteritems():
+                    # inject props, task_defn, split_data into ``self``
                     setattr(self, key, value)
         else:
             # create dataset if not loaded
@@ -25,15 +31,11 @@ class ShapesQADataset(Dataset):
             }
             data_verbose = list(itertools.product(*self.props.values()))
 
-            # select train_size for train
-            num_inst = len(data_verbose)
-            num_inst_train = int(opt['train_split'] * num_inst)
-            num_inst_test = num_inst - num_inst_train
-
-            # randomly select test
+            # randomly select train and rest of it is test
             self.split_data = {}
-            self.split_data['test'] = random.sample(data_verbose, num_inst_test)
-            self.split_data['train'] = list(set(data_verbose) - set(self.split_data['test']))
+            self.split_data['train'] = random.sample(data_verbose,
+                                                     int(self.train_split * len(data_verbose)))
+            self.split_data['test'] = list(set(data_verbose) - set(self.split_data['train']))
 
             self.task_defn = [[0, 1], [1, 0], [0, 2],
                               [2, 0], [1, 2], [2, 1],
@@ -42,95 +44,80 @@ class ShapesQADataset(Dataset):
             to_save = {
                 'props': self.props,
                 'task_defn': self.task_defn,
-                'split': self.split_data
+                'split_data': self.split_data
             }
 
-            if opt['save_path']:
-                with open(opt['save_path'], 'w') as outfile:
-                    json.dump(to_save, outfile, indent=4, separators=(',', ': '), sort_keys=True)
-
-        self.attr_val_vocab = reduce(lambda x, y: x + y,
-                                     [self.props[attr] for attr in self.props])
-        self.num_tasks = len(self.task_defn)
-
-        self.task_select = torch.LongTensor(self.task_defn)
+            with open(self.path, 'w') as outfile:
+                json.dump(to_save, outfile, indent=4, separators=(',', ': '), sort_keys=True)
 
         # number of single and pair wise tasks
         self.num_pair_tasks = 6
         self.num_single_tasks = 3
 
-        # create a vocab map for field values
-        attr_vals = reduce(lambda x, y: x + y,
-                           [self.props[attr] for attr in self.props])
-        self.attr_vocab = {value: ii for ii, value in enumerate(attr_vals)}
-        self.inv_attr_vocab = {index: attr for attr, index in self.attr_vocab.items()}
+        # create a vocab map for field values (associate each attribute value with a number)
+        attr_vals = reduce(lambda x, y: x + y, self.props.values())
+        self.vocab_attr = {index: value for index, value in enumerate(attr_vals)}
+        inv_vocab_attr = {value: index for index, value in self.vocab_attr.items()}
 
         # get encoding for attribute pairs
-        self.attr_pair = itertools.product(attr_vals, repeat=2)
-        self.attr_pair_vocab = {value: ii for ii, value in enumerate(self.attr_pair)}
-        self.inv_attr_pair_vocab = {index: value for value, index
-                                    in self.attr_pair_vocab.items()}
+        attr_pair = itertools.product(attr_vals, repeat=2)
+        self.vocab_attr_pair = {index: value for index, value in enumerate(attr_pair)}
 
         # Separate data loading for test/train
         self.data = {}
         for dtype in ['train', 'test']:
             ddata = torch.LongTensor(len(self.split_data[dtype]), len(self.props))
-            for ii, attr_set in enumerate(self.split_data[dtype]):
-                ddata[ii] = torch.LongTensor([self.attr_vocab[at] for at in attr_set])
+            for index, attr_set in enumerate(self.split_data[dtype]):
+                ddata[index] = torch.LongTensor([inv_vocab_attr[attr] for attr in attr_set])
             self.data[dtype] = ddata
+        del self.split_data
 
-        self.range_inds = torch.arange(0, len(self.split_data['train'])).long()
-        # ship to gpu if needed
-        if opt['use_gpu']:
-            for key, value in self.data.iteritems():
-                self.data[key] = value.cuda()
-            self.range_inds = self.range_inds.cuda()
+        self.range_inds = torch.arange(0, len(self.data['train'])).long()
 
     def __len__(self):
         return len(self.data['train']) + len(self.data['test'])
 
-    # get a batch
     def __getitem__(self, index):
-        task = random.nextint(0, self.num_pair_tasks - 1)
+        task = torch.Tensor([random.randint(0, self.num_pair_tasks - 1)]).long()
         example = self.data['train'][index]
 
         # now sample predictions based on task
-        select_index = self.task_select[task]
-        labels = example.gather(1, select_index)
+        select_index = torch.Tensor(self.task_defn[task[0]]).long()
+        labels = example.gather(0, select_index)
 
-        return example, task, labels
+        return {'example': example, 'task': task, 'labels': labels}
 
     # converting to text
     def reformat_talk(self, talk, preds, images, tasks, labels):
         script = []
-        if self.opt['q_out_vocab'] < 4:
-            a_vocab = [str(ii) for ii in xrange(self.opt['a_out_vocab'])]
-            q_vocab = [chr(ii + 88) for ii in xrange(self.opt['q_out_vocab'])]
+        if self.q_out_vocab < 4:
+            a_vocab = [str(ii) for ii in xrange(self.a_out_vocab)]
+            q_vocab = [chr(ii + 88) for ii in xrange(self.q_out_vocab)]
         else:
-            a_vocab = ['a-%d' % ii for ii in xrange(self.opt['a_out_vocab'])]
-            q_vocab = ['q-%d' % ii for ii in xrange(self.opt['q_out_vocab'])]
+            a_vocab = ['a-%d' % ii for ii in xrange(self.a_out_vocab)]
+            q_vocab = ['q-%d' % ii for ii in xrange(self.q_out_vocab)]
 
         attr_name_task_defn = {0: 'color', 1: 'shape', 2: 'style'}
-        for ii in xrange(images.size(0)):
+        for i in xrange(images.size(0)):
             # conversation
             conv = {}
-            conv['image'] = [self.inv_attr_vocab[jj] for jj in images[ii]]
-            conv['gt'] = [self.inv_attr_vocab[labels[ii, jj]] for jj in xrange(2)]
-            conv['task'] = [attr_name_task_defn[jj] for jj in self.task_defn[tasks[ii]]]
-            conv['pred'] = [self.inv_attr_vocab[preds[jj].data[ii, 0]]
-                            for jj in xrange(2)]
-            conv['chat'] = [q_vocab[talk[0].data[ii]],
-                            a_vocab[talk[1].data[ii]]]
+            conv['image'] = [self.vocab_attr[j] for j in images[i]]
+            conv['gt'] = [self.vocab_attr[labels[i, j]] for j in xrange(2)]
+            conv['task'] = [attr_name_task_defn[j] for j in self.task_defn[tasks[i]]]
+            conv['pred'] = [self.vocab_attr[preds[j].data[i, 0]]
+                            for j in xrange(2)]
+            conv['chat'] = [q_vocab[talk[0].data[i]],
+                            a_vocab[talk[1].data[i]]]
             if len(talk) > 3:
-                conv['chat'].extend([q_vocab[talk[2].data[ii]],
-                                     a_vocab[talk[3].data[ii]]])
+                conv['chat'].extend([q_vocab[talk[2].data[i]],
+                                     a_vocab[talk[3].data[i]]])
             script.append(conv)
 
         # re-arrange such that negative examples are on the top
         wrong_ex = []
-        for ii in script:
-            if ii['gt'] != ii['pred']:
-                wrong_ex.append(ii)
+        for i in script:
+            if i['gt'] != i['pred']:
+                wrong_ex.append(i)
 
         # remove wrong Ex from script
         for ex in wrong_ex:
@@ -152,11 +139,5 @@ class ShapesQADataset(Dataset):
 
 
 if __name__ == '__main__':
-    options = {
-        'save_path': 'data/toy64_split_0.8.json',
-        'train_split': 0.8,
-        'q_out_vocab': 3,
-        'a_out_vocab': 4,
-        'use_gpu': True
-    }
-    dataset = ShapesQADataset(options)
+    dataset = ShapesQADataset('data/toy64_split_0.8.json', 0.8, 3, 4)
+    print(random.choice(dataset))
